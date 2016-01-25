04c0250474c9b66bcbb410de4bd32db68fe655a9
Cyril Picard <Cyril@picard.ch>
2016-01-25 22:31:48 +0100

    Do not update body directly by use copy variable

--- lib/page_layout/base.rb
+++ lib/page_layout/base.rb
@@ -7,27 +7,27 @@ module PageLayout
       @main_field = false
       @main_options = {}
     end
-    
+
     def main(options = {})
       @main_field = true
       @main_options = options
     end
-    
+
     def additional(key, options = {})
       return if key == :body
       @fields[key.to_sym] = options
     end
-    
-    def parse(body)
+
+    def parse(content)
       params = {}
-      body.gsub!(/<% content_for :(?<key>[a-z0-9_]+) do %>(?:\r\n|\r|\n)(?<content>.*)(?:\r\n|\r|\n)<% end #content_\k<key> %>(?:\r\n|\r|\n)/m) do |match|
+      body = content.gsub(/<% content_for :(?<key>[a-z0-9_]+) do %>(?:\r\n|\r|\n)(?<content>.*)(?:\r\n|\r|\n)<% end #content_\k<key> %>(?:\r\n|\r|\n)/m) do |match|
         params[$1.to_sym] = $2
         ""
       end
       params[:body] = body if @main_field
       params
     end
-    
+
     def generate(params)
       body = ""
       @fields.select {|k,v| params.has_key?(k) } .each_key do |k|
@@ -38,6 +38,6 @@ module PageLayout
       body << params[:body] if @main_field
       body
     end
-    
+
   end
 end
